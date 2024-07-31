4d67f84fce9b97de3f97a7f033f659026ed74586
Sapan Mittal <sapanmittal.ece24@jecrc.ac.in>
2024-07-31 13:00:52 +0530

    Add initial files

---
+++ src/My components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an About component
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque et inventore repellat eos, beatae, earum quisquam suscipit ab officia deserunt consectetur corporis, ea praesentium odio sapiente explicabo harum labore omnis expedita maxime asperiores. Similique, minus!</p>
+    </div>
+  )
+}
